91d7bace3bf6753d860f29f515132ee536a45895
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-14 22:15:31 +0100

    Pass data to views

--- base/views.py
+++ base/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 rooms =[
-    {'id':1, 'name': "Learn python"}
+    {'id':1, 'name': "Learn python"},
+    {'id':2, 'name': "Javascript in action"},
+    {'id':3, 'name': "Frontend devs"},
 ]
 
 def home(request):
-    return render(request, 'base/home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 def room(request):
     return render(request, 'base/room.html' )
