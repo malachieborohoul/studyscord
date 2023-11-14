348777187d231a937eddb223924ec19c7c491823
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-14 10:44:54 +0100

    Establish view

--- studyscord/urls.py
+++ studyscord/urls.py
@@ -16,15 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('Home page')
-def room(request):
-    return HttpResponse('Room')
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home), 
+    path('', home),
     path('room/', room)
+ 
 ]
