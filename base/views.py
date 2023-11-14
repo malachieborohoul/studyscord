e97f6674e4a280a77d772089e310038c15b34e68
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-14 22:33:48 +0100

    Pass data to views

--- base/views.py
+++ base/views.py
@@ -16,5 +16,5 @@ def room(request, pk):
     for i in rooms:
         if i['id']==int(pk):
             room = i
-    context = {'room', room}
+    context = {'room': room}
     return render(request, 'base/room.html', context )
