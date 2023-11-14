d09f84a28519729893a98212583ff6d74f7674cf
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-14 11:12:34 +0100

    Create urls in base app

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('', views.home),
-    path('room/', views.room)
+    path('', views.home, name="home"),
+    path('room/', views.room, name="room")
 ]
