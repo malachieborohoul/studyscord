4106900f2a9452aac03412f8e2b3ebd9dcac7fd6
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-11-14 11:26:26 +0100

    Navbar in Home

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse('Home')
+    return render(request,'home.html')
 def room(request):
-    return HttpResponse('Room')
+    return render(request, 'room.html')
